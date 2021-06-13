094608551e8b25ad882d7f54e35050703ed598f7
Diego-Aero <85786727+Diego-Aero@users.noreply.github.com>
2021-06-13 17:17:21 +0200

    Problema 2

--- ps2.py
+++ ps2.py
@@ -185,7 +185,15 @@ class Robot(object):
         room:  a RectangularRoom object.
         speed: a float (speed > 0)
         """
-        raise NotImplementedError
+        self.room=room
+        pos=room.getRandomPosition()
+        self.position=pos
+        #self.position = self.room.getRandomPosition()
+        room.cleanTileAtPosition(pos)
+        #self.room.cleanTileAtPosition(self.position)
+        self.speed=speed
+        self.direction=random.randint(0, 359)
+        
 
     def getRobotPosition(self):
         """
@@ -193,7 +201,7 @@ class Robot(object):
 
         returns: a Position object giving the robot's position.
         """
-        raise NotImplementedError
+        return self.position
     
     def getRobotDirection(self):
         """
@@ -202,7 +210,7 @@ class Robot(object):
         returns: an integer d giving the direction of the robot as an angle in
         degrees, 0 <= d < 360.
         """
-        raise NotImplementedError
+        return self.direction
 
     def setRobotPosition(self, position):
         """
@@ -210,7 +218,7 @@ class Robot(object):
 
         position: a Position object.
         """
-        raise NotImplementedError
+        self.position=position
 
     def setRobotDirection(self, direction):
         """
@@ -218,7 +226,7 @@ class Robot(object):
 
         direction: integer representing an angle in degrees
         """
-        raise NotImplementedError
+        self.direction=direction
 
     def updatePositionAndClean(self):
         """
